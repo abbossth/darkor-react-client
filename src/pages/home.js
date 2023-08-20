e81cd927db56c9cc2efbe95feab825f4309e0747
abbossth <bosskodevelopment@gmail.com>
2023-08-20 18:15:55 +0500

    template is done

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,6 +13,27 @@ const Home = () => {
   return (
     <div>
       <h1>Home page</h1>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {categories &&
+          categories.map((c) => (
+            <div>
+              <img src={c.image} alt={"category photo"} width={300} />
+              <h3>{c.name}</h3>
+            </div>
+          ))}
+      </div>
+      <hr />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {products &&
+          products.map((p) => (
+            <div>
+              <img src={p.image} alt={"product photo"} width={300} />
+              <h3>{p.name}</h3>
+              <p>{p.price} UZS</p>
+              <p>{p.description}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
