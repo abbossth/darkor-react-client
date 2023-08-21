aa4729d8faf43721142da840c7807b309156f836
abbossth <bosskodevelopment@gmail.com>
2023-08-21 18:39:05 +0500

    Changed

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,35 +16,10 @@ const Home = () => {
 
   return (
     <div>
-<<<<<<< HEAD
       <Banner />
       <Collections />
       <PopularProducts />
       <BlockCover />
-=======
-      <h1>Home page</h1>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {categories &&
-          categories.map((c) => (
-            <div>
-              <img src={c.image} alt={"category photo"} width={300} />
-              <h3>{c.name}</h3>
-            </div>
-          ))}
-      </div>
-      <hr />
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {products &&
-          products.map((p) => (
-            <div>
-              <img src={p.image} alt={"product photo"} width={300} />
-              <h3>{p.name}</h3>
-              <p>{p.price} UZS</p>
-              <p>{p.description}</p>
-            </div>
-          ))}
-      </div>
->>>>>>> orign/master
     </div>
   );
 
